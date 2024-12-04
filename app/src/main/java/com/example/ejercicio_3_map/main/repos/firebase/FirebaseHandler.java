20dc2f02e1f76818e85d0f1c283caa90d57779ce
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-12-04 15:46:06 +0100

    login-registro

--- app/src/main/java/com/example/ejercicio_3_map/main/repos/firebase/FirebaseHandler.java
+++ app/src/main/java/com/example/ejercicio_3_map/main/repos/firebase/FirebaseHandler.java
@@ -1,4 +1,75 @@
 package com.example.ejercicio_3_map.main.repos.firebase;
 
+import android.util.Log;
+
+import com.example.ejercicio_3_map.main.model.Credenciales;
+import com.example.ejercicio_3_map.main.model.Ubicacion;
+import com.example.ejercicio_3_map.main.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.firestore.*;
+
 public class FirebaseHandler {
+    private FirebaseFirestore db;
+
+    public FirebaseHandler() {
+        this.db = FirebaseFirestore.getInstance();
+    }
+
+    // CRUD --> Usuarios
+    public void guardarUsuario(Usuario usuario, OnCompleteListener<Void> onCompleteListener) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("usuarios")
+                .document(usuario.getId())
+                .set(usuario)
+                .addOnCompleteListener(onCompleteListener)
+                .addOnFailureListener(e -> {
+                    Log.e("FirebaseHandler", "Error al guardar usuario", e);
+                });
+    }
+
+    public void obtenerUsuario(String id, OnCompleteListener<DocumentSnapshot> listener) {
+        db.collection("usuarios").document(id).get().addOnCompleteListener(listener);
+    }
+
+    public void eliminarUsuario(String id) {
+        db.collection("usuarios").document(id).delete()
+                .addOnSuccessListener(aVoid -> System.out.println("Usuario eliminado con éxito"))
+                .addOnFailureListener(e -> System.err.println("Error al eliminar usuario: " + e.getMessage()));
+    }
+
+    public void guardarCredenciales(Credenciales credenciales, OnCompleteListener<Void> onCompleteListener) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("credenciales")
+                .document(String.valueOf(credenciales.getId())) // Usar el ID como clave del documento
+                .set(credenciales)
+                .addOnCompleteListener(onCompleteListener)
+                .addOnFailureListener(e -> {
+                    Log.e("FirebaseHandler", "Error al guardar credenciales", e);
+                });
+    }
+
+    public void obtenerUsuarioPorCorreo(String email, OnCompleteListener<QuerySnapshot> listener) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("usuarios")
+                .whereEqualTo("correo", email)
+                .get()
+                .addOnCompleteListener(listener);
+    }
+
+    public void obtenerCredenciales(String idCredenciales, OnCompleteListener<DocumentSnapshot> listener) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("credenciales")
+                .document(idCredenciales)
+                .get()
+                .addOnCompleteListener(listener);
+    }
+
+    public void guardarUbicacion(String userId, Ubicacion ubicacion, OnCompleteListener<Void> listener) {
+        db.collection("ubicaciones")
+                .document(userId)
+                .set(ubicacion)
+                .addOnCompleteListener(listener)
+                .addOnFailureListener(e -> Log.e("FirebaseHandler", "Error al guardar ubicación", e));
+    }
+
 }
